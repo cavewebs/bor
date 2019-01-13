3551ffb2a75b4991efa4e239286449e2cc154409
Tim Chosen <timchosen@gmail.com>
2019-01-14 00:47:23 +0100

    the latest d.php and db

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "soccer_api2";
+$username = "phpmyadmin";
+$password = "ttsmuj";
+$dbname = "soccer_api";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
